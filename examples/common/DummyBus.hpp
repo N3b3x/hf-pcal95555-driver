ba77ee11a6a72f9dd963bb713c61cdf8a07e1455
Nebiyu Tadesse <nebysma@gmail.com>
2025-06-15 09:50:29 -0600

    Add ESP-IDF example and CI

---
+++ examples/common/DummyBus.hpp
@@ -0,0 +1,9 @@
+#include "../..//src/pacl95555.hpp"
+
+class DummyBus : public PACL95555::i2cBus {
+public:
+  bool write(uint8_t, uint8_t, const uint8_t *, size_t) override {
+    return true;
+  }
+  bool read(uint8_t, uint8_t, uint8_t *, size_t) override { return true; }
+};
---
+++ examples/esp32/main/main.cpp
@@ -0,0 +1,10 @@
+#include "../../common/DummyBus.hpp"
+#include "pacl95555.hpp"
+
+extern "C" void app_main(void) {
+  DummyBus bus;
+  PACL95555 gpio(&bus, 0x20);
+  gpio.resetToDefault();
+  gpio.setPinDirection(0, GPIODir::Output);
+  gpio.writePin(0, true);
+}
